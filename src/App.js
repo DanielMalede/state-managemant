fa2abd506d749ff3e9b8b8793540a2c15e52f288
DanielMalede <danielmalede@gmail.com>
2022-08-31 23:02:14 +0300

    /

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useReducer } from 'react';
 import './App.css';
-import { UP_ACTION, DOWN_ACTION, RESET_ACTION } from './store/actions/counter.action';
+import { UP_ACTION, DOWN_ACTION, RESET_ACTION ,RANDOM_ACTION } from './store/actions/counter.action';
 import reducer from './store/reducers/counter-reducer';
 let initialstate = 0
 function App() {
@@ -11,6 +11,7 @@ function App() {
       <br></br>
       <button onClick={() => dispatch({ type: UP_ACTION })}>Up</button>
       <button onClick={() => dispatch({ type: DOWN_ACTION })}>down</button>
+      <button onClick={() => dispatch({ type: RESET_ACTION })}>reset</button>
       <button onClick={() => dispatch({ type: RESET_ACTION })}>reset</button>
     </div>
   );
--- src/store/reducers/counter-reducer.js
+++ src/store/reducers/counter-reducer.js
@@ -1,5 +1,4 @@
-import { UP_ACTION, DOWN_ACTION, RESET_ACTION } from "../actions/counter.action"
-
+import { UP_ACTION, DOWN_ACTION, RESET_ACTION, RANDOM_ACTION } from "../actions/counter.action"
 export default function reducer(state, action) {
     switch (action.type) {
         case UP_ACTION:
@@ -8,6 +7,8 @@ export default function reducer(state, action) {
             return state - 1
         case RESET_ACTION:
             return 0
+        case RANDOM_ACTION:
+            return Math.floor(Math.random() * 101)
         default:
             return state
     }
