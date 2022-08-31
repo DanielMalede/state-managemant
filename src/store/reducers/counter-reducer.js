7fed7a13ab0e8e98576370172303886d791df3f2
DanielMalede <danielmalede@gmail.com>
2022-08-31 22:45:00 +0300

    .

--- src/store/reducers/counter-reducer.js
+++ src/store/reducers/counter-reducer.js
@@ -1,15 +1,15 @@
-import { UP_ACTION,DOWN_ACTION,RESET_ACTION } from "../actions/counter.action"
+import { UP_ACTION, DOWN_ACTION, RESET_ACTION } from "../actions/counter.action"
 
-export default function reducer (state, action){
+export default function reducer(state, action) {
     switch (action.type) {
-      case UP_ACTION:
-        return state + 1
-      case DOWN_ACTION:
-        return state - 1
-      case RESET_ACTION:
-        return  0
-      default:
-        return state
+        case UP_ACTION:
+            return state + 1
+        case DOWN_ACTION:
+            return state - 1
+        case RESET_ACTION:
+            return 0
+        default:
+            return state
     }
-  
-  }
+
+}
