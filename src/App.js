2465b2d4b6df1c42688f9b059c9b46ed6ced59c2
DanielMalede <danielmalede@gmail.com>
2022-09-01 11:38:52 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import { useReducer, useRef } from 'react';
 import './App.css';
-import { updateCounter, downAction, upAction, resetAction, randomAction } from './store/actions/counter.action';
+import { userNumberCounterAction, downCounterAction, upCounterAction, resetCounterAction, randomCounterAction } from './store/actions/counter.action';
 import reducer from './store/reducers/counter-reducer';
 let initialstate = 0
 function App() {
   const [state, dispatch] = useReducer(reducer, initialstate);
   const userNumber = useRef(0)
-  console.log(userNumber.current.value);
   return (
     <div className="App">
       {state}
       <br></br>
-      <button onClick={() => dispatch(upAction())}>Up</button>
-      <button onClick={() => dispatch(downAction())}>down</button>
-      <button onClick={() => dispatch(resetAction())}>reset</button>
-      <button onClick={() => dispatch(randomAction())}>random </button>
-      <button onClick={() => dispatch(updateCounter(+userNumber.current.value))}>user Number </button>
+      <button onClick={() => dispatch(upCounterAction())}>Up</button>
+      <button onClick={() => dispatch(downCounterAction())}>down</button>
+      <button onClick={() => dispatch(resetCounterAction())}>reset</button>
+      <button onClick={() => dispatch(randomCounterAction())}>random </button>
+      <button onClick={() => dispatch(userNumberCounterAction(+userNumber.current.value))}>user Number </button>
       <br></br>
       <input ref={userNumber} type="number" />
     </div>
--- src/store/actions/counter.action.js
+++ src/store/actions/counter.action.js
@@ -1,21 +1,21 @@
 import { UPDATE_COUNTER, DOWN_ACTION, UP_ACTION, RESET_ACTION, RANDOM_ACTION } from "./counter-type"
 
-export const updateCounter = (newNumber) => {
+export const userNumberCounterAction = (newNumber) => {
     return { type: UPDATE_COUNTER, payload: newNumber }
 }
 
-export const downAction = (newNumber) => {
+export const downCounterAction = (newNumber) => {
     return { type: DOWN_ACTION, payload: newNumber }
 }
 
-export const upAction = (newNumber) => {
+export const upCounterAction = (newNumber) => {
     return { type: UP_ACTION, payload: newNumber }
 }
 
-export const resetAction = (newNumber) => {
+export const resetCounterAction = (newNumber) => {
     return { type: RESET_ACTION, payload: newNumber }
 }
 
-export const randomAction = (newNumber) => {
+export const randomCounterAction = (newNumber) => {
     return { type: RANDOM_ACTION, payload: newNumber }
 }
