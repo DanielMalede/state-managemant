f7e41ac18940a20816bf8ba096d9888d75a2b677
DanielMalede <danielmalede@gmail.com>
2022-09-01 15:54:44 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import Counter from './components/pages/Counter/Counter';
-import CounterProvider from './context/Counter-Context/Counter-Context';
 import Home from './components/pages/Home/Home'
 function App() {
   return (
